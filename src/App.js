1e76588fc97ca2a8cb9c5086dc9ddb84b5b0c04d
lonadels <lonadels@ya.ru>
2020-01-22 08:10:08 +0300

    main commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,143 @@
-import React, { useState, useEffect } from 'react';
-import connect from '@vkontakte/vk-connect';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import React, { Component } from 'react';
+
+//import connect from '@vkontakte/vk-connect';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+import { ConfigProvider, Root, View, Panel, ScreenSpinner, Div, Group, List, Cell } from '@vkontakte/vkui';
+import { PanelHeader, Button } from '@vkontakte/vkui';
 
-const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+import connect from '@vkontakte/vkui-connect-mock';
+import { createStore } from 'redux';
 
-	useEffect(() => {
-		connect.subscribe(({ detail: { type, data }}) => {
+
+const reducer = (state = { groups: {}, users: {} }, action) => {
+	switch (action.type) {
+		case "ADD_GROUP":
+			let groupKeys = Object.keys(state.groups);
+			return {
+				...state,
+				groups: {
+					...state.groups,
+					[2^groupKeys.length]: {
+						name: action.name,
+						permissions: []
+					}
+				}
+			}
+		case "ADD_PERMISSION":
+			let group = state.groups[action.groupID];
+			return {
+				...state,
+				groups: {
+					...state.groups, [action.groupID]:
+					{
+						...group,
+						permissions: [...group.permissions, action.permission]
+					}
+				}
+			}
+		case "ADD_USER":
+			return { ...state, users: [...state.users, action.user] }
+		default: return state;
+	}
+}
+
+var store = createStore(reducer);
+
+class User {
+	constructor(name, privLevel) {
+		this._name = name;
+		this._privLevel = privLevel;
+	}
+
+	hasPermission(permission) {
+		let permissions = [];
+
+		for (let [group, bin] in Object.entries(store.getState().groups))
+			if (bin & this.privLevel)
+				permissions = [...permissions, ...store.getState().permissions[group]]
+
+		return permissions.indexOf(permission) !== -1 && permissions.indexOf("-" + permission) === -1;
+	}
+}
+
+export default class App extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			user: null,
+			popout: <ScreenSpinner size='large' />,
+			groups: []
+		}
+
+		this.fetchData();
+
+		this.addUser = this.addUser.bind(this);
+		this.addGroup = this.addGroup.bind(this);
+		this.addPermission = this.addPermission.bind(this);
+
+		store.subscribe(()=>{
+			this.setState({groups: Object.values(store.getState().groups)})
+		})
+	}
+
+	async fetchData() {
+		const user = await connect.send('VKWebAppGetUserInfo');
+		this.setState({ user, popout: null });
+	}
+
+	addUser() {
+		//let user = new User("lonadels", );
+		//user.
+		//store.dispatch({type: "ADD_USER", user });
+	}
+
+	addGroup() {
+		let name = "test";
+		store.dispatch({ type: "ADD_GROUP", name });
+	}
+
+	addPermission() {
+		//let user = new User("lonadels", );
+		//user.
+		//store.dispatch({type: "ADD_USER", user });
+	}
+
+	componentDidUpdate() {
+		connect.subscribe(({ detail: { type, data } }) => {
 			if (type === 'VKWebAppUpdateConfig') {
 				const schemeAttribute = document.createAttribute('scheme');
 				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
 				document.body.attributes.setNamedItem(schemeAttribute);
 			}
 		});
-		async function fetchData() {
-			const user = await connect.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+	}
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
 
-	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
-		</View>
-	);
+	render() {
+		
+		return (
+			<ConfigProvider>
+				<Root>
+					<View popout={this.state.popout}>
+						<Panel>
+							<PanelHeader>JSPermission</PanelHeader>
+							<Group>
+								<Div>
+									<List>
+										{ this.state.groups.map((group, i) => <Cell key={i}>{group.name}</Cell>) }
+									</List>
+									<Button onClick={this.addGroup}>Добавить группу</Button>
+								</Div>
+							</Group>
+
+						</Panel>
+					</View>
+				</Root>
+			</ConfigProvider>
+		);
+	}
 }
-
-export default App;
 
